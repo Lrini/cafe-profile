2b60f56a8f9dc110dda6b4accb1ee22be6444c99
Lrini <iwangzr94@gmail.com>
2022-06-08 21:51:52 +0800

    keempat

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
           <li><a class="nav-link scrollto" href="#menu">Menu</a></li>
-          <li><a class="nav-link scrollto" href="#specials">Order</a></li>
+          <li><a class="nav-link scrollto" href="#order">Order</a></li>
     </div>
   </header><!-- End Header -->
 
@@ -78,7 +78,7 @@
                             $res = $conn->query("select * from menu");
                             while($row = $res->fetch_assoc()){
                             echo '
-                            <div class="col-lg-6 menu-item filter-starters">
+                            <div class="col-lg-12 menu-item filter-starters">
                               <img src="admin/menu/'.$row['gambar'].'" class="menu-img">
                               <div class="menu-content">
                                 '.$row['menu'].'</a><span> Rp.'.$row['harga'].'</span>
@@ -93,9 +93,41 @@
                               ?>
 
         </div>
+        
 
       </div>
     </section><!-- End Menu Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="order" class="order">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Order</h2>
+          <p>Contact Us</p>
+        </div>
+      </div>
+      <div class="container" data-aos="fade-up">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                  <div class="row">
+                    <div class="col-md-6 form-group">
+                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                    </div>
+                    <div class="col-md-6 form-group mt-3 mt-md-0">
+                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Phone number" required>
+                    </div>
+                  </div>
+                  <div class="form-group mt-3">
+                    <textarea class="form-control" name="message" rows="8" placeholder="Message" required></textarea>
+                  </div>
+                  <div class="text-center"><button type="submit" class="btn btn-primary">Send Message</button></div>
+                </form>
+        
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
 
   </main><!-- End #main -->
 
